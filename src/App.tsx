3f0f3fd0b3d83804352a6afbfdac7cb7a0955fcb
Minh Tung <tungho.mth@gmail.com>
2025-09-26 12:34:52 +0700

    fe

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/dienlanhvannhat">
       <AuthProvider>
         <ScrollToTop />
         <AppRoutes />
